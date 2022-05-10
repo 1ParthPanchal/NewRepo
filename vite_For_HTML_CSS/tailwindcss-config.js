f18bcc0cefbf66c065bd421f247807f147a008d2
Parth Panchal <ppanchal1090@gmail.com>
2022-05-10 14:06:07 +0530

    commit changes

---
+++ vite_For_HTML_CSS/tailwindcss-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ["./Tailwind/**/*.{html,js}", './node_modules/tw-elements/dist/js/**/*.js'],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#FF6363',
+                secondary: {
+                    100: '#E2E2D5',
+                    200: '#888883'
+                }
+            }
+        },
+    },
+    plugins: [require('tw-elements/dist/plugin')],
+}
